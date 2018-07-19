8174d27733a2ae881eba76d7642780f6e9393ff0
IIEpicSkillzz <40809642+IIEpicSkillzz@users.noreply.github.com>
2018-07-19 14:10:41 -0700

    IT IS WORKIMN NOW I NEED TO ADD LINKS
    TIME TO ADD LINK

--- Komic Final/script.js
+++ Komic Final/script.js
@@ -1,8 +1,8 @@
 var comics = [
-  {hero:"superman", comicName: "The Death of Superman"},
-  {hero:"superman", comicName: "The Trial of Superman"},
-  {hero:"superman", comicName: "Reign of Doomsday"},
-  {hero:"batman", comicName: "Beyond"}
+  {hero:"Superman", comicName: "The Death of Superman", imgUrl:"https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/359464/359464._SX360_QL80_TTD_.jpg"},
+  {hero:"Superman", comicName: "The Trial of Superman", imgUrl:"https://static.comicvine.com/uploads/scale_large/0/4/73792-3816-70811-1-superman.jpg"},
+  {hero:"Superman", comicName: "Reign of Doomsday", imgUrl:"https://i.pinimg.com/originals/94/38/32/943832e4ee1ffed90996d068a37dddf1.jpg"},
+  {hero:"Batman", comicName: "Beyond", imgUrl:"http://neoncinemas.com/wp-content/uploads/2016/10/Batman-Beyond-1-DC-Comics-Rebirth-spoilers-1.jpg"}
 ];
 
 localStorage = window.localStorage;
